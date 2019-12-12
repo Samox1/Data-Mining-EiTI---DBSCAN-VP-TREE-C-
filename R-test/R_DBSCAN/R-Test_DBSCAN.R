18cf6c9b121fbc40ef6f6d093c6a26e4f8863492
Szymon Baczynski <simon.baczynski@gmail.com>
2019-12-12 18:44:45 +0100

    wtf

--- R-test/R_DBSCAN/R-Test_DBSCAN.R
+++ R-test/R_DBSCAN/R-Test_DBSCAN.R
@@ -87,13 +87,14 @@ library(readr)
 # }
 
 
-# Data_Cluster <- read_csv("D:/Programming/Data-Mining-EiTI---DBSCAN-VP-TREE-C-/Data_Cluster.csv",)
-Data_Cluster <- as.data.frame(read.table("D:/Programming/Data-Mining-EiTI---DBSCAN-VP-TREE-C-/Data_Cluster.csv", header=FALSE,sep=","))
+Data_Cluster <- as.data.frame(read.table("~/GitHub/DBSCAN_VP-TREE_Cpp/Data_Cluster.csv", header=FALSE,sep=","))
+#Data_Cluster <- as.data.frame(read.table("D:/Programming/Data-Mining-EiTI---DBSCAN-VP-TREE-C-/Data_Cluster.csv", header=FALSE,sep=","))
 
 cluster <- as.factor(Data_Cluster[,3])
 
 library(ggplot2)
-sp <- ggplot(Data_Cluster[,1:2], aes(x=V1, y=V2, colour = cluster, size = cluster)) + geom_point() + geom_point(size = 50, pch = 1) + coord_cartesian(xlim =c(-1, 8), ylim = c(-1, 8))
+#sp <- ggplot(Data_Cluster[,1:2], aes(x=V1, y=V2, colour = cluster, size = cluster)) + geom_point() + geom_point(size = 50, pch = 1) + coord_cartesian(xlim =c(-1, 8), ylim = c(-1, 8))
+sp <- ggplot(Data_Cluster[,1:2], aes(x=V1, y=V2, colour = cluster, size = cluster)) + geom_point() + geom_point(size = 50, pch = 1)
 show(sp)
 
 # cols <- c((-1)='red',1='blue',2='green',3='yellow');
