21be85810effea315d1f3dc82cf36dccc534b1c8
Szymon Baczyński <simon.baczynski@gmail.com>
2019-12-12 22:17:12 +0100

    Uniwersalne wczytywanie - Part 1

--- dbscan.cpp
+++ dbscan.cpp
@@ -13,17 +13,24 @@ using namespace std;
 using namespace std::chrono; 
 
 
-struct Punkt{
-    double x = 0.0;
-    double y = 0.0;
-    int cluster = UNDEFINED;
+class Punkt{
+    public:
+        double *x;
+        int cluster = UNDEFINED;
+        void Przydziel(int);
+        ~Punkt();
 };
+void Punkt::Przydziel(int N){
+    x = new double[N];
+}
+Punkt::~Punkt(){
+    delete [] x;
+}
 
-
-double DistFunc(Punkt *pkt1, Punkt *pkt2);
+double DistFunc(Punkt *pkt1, Punkt *pkt2, int ile_x);
 int RangeQuery(Punkt *pkt, int *N_tab, int ile_linii, int Qindex, double Eps);
 int S_N_Merge(int *S_tab, int *N_tab, int S_licznik, int N_licznik, int ile_linii);
-void DBSCAN_Origin(Punkt *pkt, int *S_tab, int *N_tab, int Eps, int ile_linii, int minN, int C);
+void DBSCAN_Origin(Punkt *pkt, int *S_tab, int *N_tab, double Eps, int ile_linii, int minN, int C);
 
 
 int main()
@@ -35,9 +42,10 @@ int main()
     int *S_tab;         // Tablica "Seed" --> rozszerzajacy sie obszar sasiadow wokol punktu P
 
     string file1 = "DataCpp.csv";
-    string line, word, temp;
+    string line, word, temp, struktura;
     int index = 0;
     int ile_linii = 0;
+    int ile_x = 0;
 
     cout << "--- Open file: " << file1 << " --> ";
 
@@ -54,12 +62,34 @@ int main()
                 ile_linii++;
             }
         }
+
         //cout << plik.tellg() << endl;
         plik.clear();
         plik.seekg(0, ios::beg);
-        //cout << plik.tellg() << endl;
-        //cout << "Ile linii: " <<  ile_linii << endl;
-        pkt = new Punkt[ile_linii];
+
+        while(getline(plik, line, '\n'))
+        {
+            stringstream sstream(line);
+            while(getline(sstream, word, ','))
+            {
+                // cout << word << " ";
+                struktura = struktura + word + " | ";
+                ile_x++;
+            }
+            break;
+        }
+        // cout << "Ile kolumn: " << ile_x << endl;
+        plik.clear();
+        plik.seekg(0, ios::beg);
+        // cout << plik.tellg() << endl;
+        
+        // cout << "Wierszy w pliku: " <<  ile_linii << endl;
+
+        pkt = new Punkt[ile_linii];                             // Inicjalizacja pamieci
+
+        for(int i=0; i<ile_linii; i++){
+            pkt[i].Przydziel(2);
+        }
 
         int ktora_linia = 0;
 
@@ -73,10 +103,10 @@ int main()
                 while (getline(sstream, word, ','))
                 {
                     if(index==1){
-                        pkt[ktora_linia].x = atof(word.c_str());
+                        pkt[ktora_linia].x[0] = atof(word.c_str());
                         //cout << "X: " << word << " ";
                     }else if(index==2){
-                        pkt[ktora_linia].y = atof(word.c_str());
+                        pkt[ktora_linia].x[1] = atof(word.c_str());
                         //cout << "Y: " << word << " ";
                     }
                     index++;
@@ -91,10 +121,10 @@ int main()
     plik.close();
     cout << "Close file: Imported 2D data" << endl;
 
+    cout << "***" << endl << "File Structure: " << endl << "Columns: " <<  ile_x << endl << "Rows: " << ile_linii << endl << "Structure: " << struktura << endl << "***" << endl ;
 // Show imported data
-
     // for(int i=0; i<ile_linii; i++){
-    //     cout << "x: " << pkt[i].x << " | y:" << pkt[i].y << endl;
+    //     cout << "x: " << pkt[i].x[0] << " | y:" << pkt[i].x[1] << endl;
     // }
 
 
@@ -117,65 +147,8 @@ int main()
 
 auto start = high_resolution_clock::now();      // Time - START
 
-DBSCAN_Origin(pkt, S_tab, N_tab, Eps, ile_linii, minN, C);
-
 // DBSCAN --- START //
-
-// for(int P = 0; P < ile_linii; P++)
-// {
-//     if(pkt[P].cluster != UNDEFINED)
-//     {
-//         continue;
-//     }
-
-//     int ile_sasiadow = RangeQuery(pkt, N_tab, ile_linii, P, Eps);
-    
-//     // cout << "Punkt: " << P << " -> x: " << pkt[P].x << " | y: " << pkt[P].y << endl;
-//     // cout << "Ile sasiadow: " << ile_sasiadow << endl << endl;
-
-//     if(ile_sasiadow < minN)
-//     {
-//         pkt[P].cluster = NOISE;             // IF: N < minN => NOISE
-//         continue;
-//     }
-
-//     C = C + 1;                              // Cluster number increment
-//     pkt[P].cluster = C;                     // ELSE: N > minN => Cluster
-    
-//     // cout << "Punkt: " << P << " | Cluster: " << pkt[P].cluster << endl;     // Show changing Cluster number
-    
-//     for(int i=0; i<ile_linii; i++)
-//     {
-//         S_tab[i] = N_tab[i];           
-//     }
-
-//     int S_licznik = ile_sasiadow;
-  
-//     for(int i=0; i<S_licznik; i++)               // For Each Point Q in S
-//     {
-//         if(S_tab[i] != -1)
-//         {
-//             if(pkt[S_tab[i]].cluster == NOISE)          // IF: Q == NOISE then Q = Cluster
-//             {
-//                 pkt[S_tab[i]].cluster = C;
-//             }
-
-//             if(pkt[S_tab[i]].cluster != UNDEFINED)      // IF: Q == NOISE or CLUSTER then leave Q
-//             {     
-//                 continue;
-//             }
-
-//             pkt[S_tab[i]].cluster = C;
-//             ile_sasiadow = RangeQuery(pkt, N_tab, ile_linii, S_tab[i], Eps);
-
-//             if( ile_sasiadow >= minN)
-//             {
-//                 S_licznik = S_N_Merge(S_tab, N_tab, S_licznik, ile_sasiadow, ile_linii);
-//             }
-//         }
-//     }
-// }
-
+DBSCAN_Origin(pkt, S_tab, N_tab, Eps, ile_linii, minN, C);
 // DBSCAN --- END //
 
 auto stop = high_resolution_clock::now();                       // Time - STOP
@@ -204,7 +177,7 @@ cout << "DBSCAN Time: " << duration.count() << " us" << endl;   // Time - show F
     {   
         for (int i = 0; i < ile_linii; i++)
         {
-            plik_out << pkt[i].x << "," << pkt[i].y << "," << pkt[i].cluster << endl;
+            plik_out << pkt[i].x[0] << "," << pkt[i].x[1] << "," << pkt[i].cluster << endl;
         }
     }else{
         cout << "Can't open file" << endl;
@@ -225,10 +198,16 @@ cout << "DBSCAN Time: " << duration.count() << " us" << endl;   // Time - show F
 // --- FUNCTION --- FUNCTION --- FUNCTION --- FUNCTION --- FUNCTION --- FUNCTION --- //
 
 // Function to calculate distance 
-double DistFunc(Punkt *pkt1, Punkt *pkt2) 
+double DistFunc(Punkt *pkt1, Punkt *pkt2, int ile_x) 
 { 
+    double distance = 0.0;
     // Calculating distance 
-    return sqrt(pow(pkt2->x - pkt1->x, 2) +  pow(pkt2->y - pkt1->y, 2) * 1.0); 
+    for(int i=0; i<ile_x; i++){
+        distance = distance + pow(pkt2->x[i] - pkt1->x[i], 2);
+    }
+    distance = sqrt(distance);
+    
+    return distance;
 } 
 
 int RangeQuery(Punkt *pkt, int *N_tab, int ile_linii, int Qindex, double Eps)
@@ -244,7 +223,7 @@ int RangeQuery(Punkt *pkt, int *N_tab, int ile_linii, int Qindex, double Eps)
     {
         if(i != Qindex)
         {
-            if(DistFunc(&pkt[Qindex], &pkt[i]) <= Eps)
+            if(DistFunc(&pkt[Qindex], &pkt[i], 2) <= Eps)
             {
                 N_tab[j] = i;
                 j++;
@@ -289,7 +268,7 @@ int S_N_Merge(int *S_tab, int *N_tab, int S_licznik, int N_licznik, int ile_lini
 
 
 // NIE DZIALA
-void DBSCAN_Origin(Punkt *pkt, int *S_tab, int *N_tab, int Eps, int ile_linii, int minN, int C)
+void DBSCAN_Origin(Punkt *pkt, int *S_tab, int *N_tab, double Eps, int ile_linii, int minN, int C)
 {
 
 for(int P = 0; P < ile_linii; P++)
