fe3df37bd4587243f7ffcf9f521477973ad83e52
Szymon Baczynski <simon.baczynski@gmail.com>
2019-12-20 13:06:47 +0100

    VP test -1

---
+++ R-test/R_DBSCAN/Algo_Test.R
@@ -0,0 +1,73 @@
+### Test VP-Tree
+
+rm(list = ls())         # Clear Environment
+gc()                    # Free up memrory and report the memory usage.
+
+
+library(tidyverse)
+library(MASS)
+library(ggplot2)
+library(ggforce)
+
+
+### Functions ###########################################################################
+
+euc.dist <- function(x1, x2){
+  sqrt(sum((x1 - x2) ^ 2))
+}
+
+dist_calc <- function(p, D){
+  dist_all <- NULL
+  for(i in 1:nrow(D)) dist_all[i] <- euc.dist(p, D[i,])
+  #cat("Dist_all = " ,dist_all, "\n")
+  
+  return(dist_all)
+}
+
+Select_VP <- function(S){
+  P <- S[sample(nrow(S), 10, replace = F), ]
+  best_spread <- 0
+  best_p <- 0
+  
+  for (px in c(1:nrow(P))) {
+    #cat("Kappa px: ",px, "\n")
+    p = as.data.frame(P[px,])
+    #show(p)
+    p_id <- as.integer(rownames(p))
+    #cat(typeof(p_id))
+    P_temp <- P[as.integer(rownames(P)) != p_id,]
+    #show(P_temp)
+    D <- P_temp[sample(nrow(P_temp), 3, replace = F), ]
+    
+    dist_all <- dist_calc(p,D)
+    
+    mu <- median(dist_all)
+    #cat("MU = ", mu, "\n")
+    
+    spread_temp <- var(dist_all - mu)
+      
+    if (spread_temp > best_spread){
+      best_spread <- spread_temp
+      best_p <- p
+    }
+    
+  }
+  return(best_p)
+}
+
+
+### Main ###############################################################################
+
+Dane <- as.data.frame(read.table("DataCpp-2D-1000.csv", header=TRUE,sep=","))
+Dane <- Dane[1:nrow(Dane),2:ncol(Dane)]
+colnames(Dane) <- c("x", "y")
+
+node.p <- Select_VP(Dane)
+Dane_temp <- Dane[as.integer(rownames(Dane)) != as.integer(rownames(node.p)),]
+node.mu <- median(dist_calc(node.p, Dane))
+
+L <- 
+R <- 
+
+
+
--- R-test/R_DBSCAN/R-Test_DBSCAN.R
+++ R-test/R_DBSCAN/R-Test_DBSCAN.R
@@ -4,6 +4,62 @@ library(readr)
 library(tidyverse)
 library(dbscan)
 
+###################################################################################
+# Plot some data for C++ project
+
+library(MASS)
+
+S <- matrix(c(1,0,0,1),2,2)
+mt1 <- c(2,2)
+mt2 <- c(6,0)
+n1 <- 1000
+n2 <- 1000
+n <- n1 + n2
+
+X1 <- mvrnorm(n1, mt1, S)
+X2 <- mvrnorm(n2, mt2, S)
+
+DATA_Cpp <- rbind(X1, X2)
+kNN1 <- kNNdistplot(DATA_Cpp, k = 5)
+abline(h=.5, col = "red", lty=2)
+
+# Write to CSV
+write.csv(DATA_Cpp, "DataCpp-2D-1000.csv")
+
+
+plot(X1, ylim = c(-5,5), xlim = c(-2,10), pch = 19, col = "blue", xlab = "X", ylab = "Y", font = 2, asp = 1)
+abline(v = 0, h = 0, col = "gray")
+points(X2, pch = 19, col = "orange")
+
+
+### Visualization
+
+Data_Cluster <- as.data.frame(read.table("Data_Clustered.csv", header=FALSE,sep=","))
+#Data_Cluster <- as.data.frame(read.table("D:/Programming/Data-Mining-EiTI---DBSCAN-VP-TREE-C-/Data_Cluster.csv", header=FALSE,sep=","))
+
+cluster <- as.factor(Data_Cluster[,3])
+
+library(ggplot2)
+library(ggforce)
+#sp <- ggplot(Data_Cluster[,1:2], aes(x=V1, y=V2, colour = cluster, size = cluster)) + geom_point() + geom_point(size = 50, pch = 1) + coord_cartesian(xlim =c(-1, 8), ylim = c(-1, 8))
+
+sp <- ggplot(Data_Cluster[,1:2], aes(x=V1, y=V2, colour = cluster)) + geom_point() + scale_x_continuous() + scale_y_continuous() #+ theme(aspect.ratio = 1)
+sp <- sp + coord_fixed(ratio = 1)
+sp <- sp + geom_point(size=10, pch=1) #+ coord_cartesian(xlim = c(0,4), ylim = c(0,4))
+show(sp)
+
+x <- c(0,1,5)
+y <- c(0,1,3)
+kappa <- as.data.frame(x)
+kappa <- cbind(kappa,y)
+sp1 <- ggplot(kappa, aes(x=x, y=y)) + geom_point() + coord_fixed(ratio = 1) + geom_circle(aes(x0=x, y0=y, r= 10), data= kappa)
+show(sp1)
+
+  # cols <- c((-1)='red',1='blue',2='green',3='yellow');
+# plot(Data_Cluster[,1:2], xlim=c(0,8), ylim=c(0,8),col=cols[Data_Cluster[3]])
+#show(3^10)
+
+
 # 
 # forestfires <- read_csv("forestfires.csv")
 # View(forestfires)
@@ -37,77 +93,3 @@ library(dbscan)
 # show(res)
 # 
 # plot(iris[,1], iris[,3])
-
-
-###################################################################################
-# Plot some data for C++ project
-
-library(MASS)
-
-S <- matrix(c(1,0,0,1),2,2)
-mt1 <- c(2,2)
-mt2 <- c(6,0)
-n1 <- 1000
-n2 <- 1000
-n <- n1 + n2
-
-X1 <- mvrnorm(n1, mt1, S)
-X2 <- mvrnorm(n2, mt2, S)
-
-DATA_Cpp <- rbind(X1, X2)
-kNN1 <- kNNdistplot(DATA_Cpp, k = 5)
-abline(h=.5, col = "red", lty=2)
-
-# Write to CSV
-write.csv(DATA_Cpp, "DataCpp-2D-1000.csv")
-
-
-plot(X1, ylim = c(-5,5), xlim = c(-2,10), pch = 19, col = "blue", xlab = "X", ylab = "Y", font = 2, asp = 1)
-abline(v = 0, h = 0, col = "gray")
-points(X2, pch = 19, col = "orange")
-
-library(readr)
-# DataCpp <- read_csv("D:/Programming/Data-Mining-EiTI---DBSCAN-VP-TREE-C-/DataCpp.csv")
-# #View(DataCpp)
-# 
-# plot(DataCpp[2:3], xlim=c(0,8), ylim=c(0,8))
-# #symbols(x=DataCpp[10,2], y=DataCpp[10,3], circles=rep(0.5), add=T, inches=F)
-# XX <- DataCpp[,2]
-# YY <- DataCpp[,3]
-# ZZ <- c(10,20,25,48,57,7,10,35,42,19,23,24,50,49,43,39,45,4,47,52,18,15,36,6,3)
-# 
-# for (val in ZZ){
-#   plot(DataCpp[2:3], xlim=c(0,8), ylim=c(0,8))
-#   symbols(x=DataCpp[val,2], y=DataCpp[val,3], circles=rep(0.5), add=T, inches=F)
-# }
-# 
-# plot(DataCpp[2:3], xlim=c(0,8), ylim=c(0,8))
-# for (val in ZZ){
-#   symbols(x=DataCpp[val,2], y=DataCpp[val,3], circles=rep(0.5), add=T, inches=F)
-# }
-
-
-Data_Cluster <- as.data.frame(read.table("Data_Clustered.csv", header=FALSE,sep=","))
-#Data_Cluster <- as.data.frame(read.table("D:/Programming/Data-Mining-EiTI---DBSCAN-VP-TREE-C-/Data_Cluster.csv", header=FALSE,sep=","))
-
-cluster <- as.factor(Data_Cluster[,3])
-
-library(ggplot2)
-library(ggforce)
-#sp <- ggplot(Data_Cluster[,1:2], aes(x=V1, y=V2, colour = cluster, size = cluster)) + geom_point() + geom_point(size = 50, pch = 1) + coord_cartesian(xlim =c(-1, 8), ylim = c(-1, 8))
-
-sp <- ggplot(Data_Cluster[,1:2], aes(x=V1, y=V2, colour = cluster)) + geom_point() + scale_x_continuous() + scale_y_continuous() #+ theme(aspect.ratio = 1)
-sp <- sp + coord_fixed(ratio = 1)
-sp <- sp + geom_point(size=10, pch=1) #+ coord_cartesian(xlim = c(0,4), ylim = c(0,4))
-show(sp)
-
-x <- c(0,1,5)
-y <- c(0,1,3)
-kappa <- as.data.frame(x)
-kappa <- cbind(kappa,y)
-sp1 <- ggplot(kappa, aes(x=x, y=y)) + geom_point() + coord_fixed(ratio = 1) + geom_circle(aes(x0=x, y0=y, r= 10), data= kappa)
-show(sp1)
-
-  # cols <- c((-1)='red',1='blue',2='green',3='yellow');
-# plot(Data_Cluster[,1:2], xlim=c(0,8), ylim=c(0,8),col=cols[Data_Cluster[3]])
-#show(3^10)
